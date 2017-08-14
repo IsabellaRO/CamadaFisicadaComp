99b627408f162f22895384fbbb1489da5a7cbdc1
eduardotp1 <edu.tirta@gmail.com>
2017-08-14 14:42:45 -0300

    client e server comunicando

---
+++ Proj-1-Comunicacao no repositório/0-COM-LoopBack/src/server.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#####################################################
+# Camada Física da Computação
+# Prof. Rafael Corsi
+#  Abril/2017
+#  Aplicação
+####################################################
+
+from enlace import *
+import time
+
+# Serial Com Port
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+#serialName = "COM3"                  # Windows(variacao de)
+serialName = "COM4"
+
+def main():
+    # Inicializa enlace
+    com = enlace(serialName)
+
+    # Ativa comunicacao
+    com.enable()
+
+# Endereco da imagem a ser transmitida
+    # imageR = "./imgs/imageE.png"
+
+    # Endereco da imagem a ser salva
+    imageW = "./imgs/recebida.png"
+
+    # Log
+    print("-------------------------")
+    print("Comunicação inicializada")
+    print("  porta : {}".format(com.fisica.name))
+    print("-------------------------")
+
+    txLen = 3093
+
+    # Faz a recepção dos dados
+    print ("Recebendo dados .... ")
+    rxBuffer, nRx = com.getData(txLen)
+
+    # log
+    print ("Lido              {} bytes ".format(nRx))
+
+    # Salva imagem recebida em arquivo
+    print("-------------------------")
+    print ("Salvando dados no arquivo :")
+    print (" - {}".format(imageW))
+    f = open(imageW, 'wb')
+    f.write(rxBuffer)
+
+    # Fecha arquivo de imagem
+    f.close()
+
+    # Encerra comunicação
+    print("-------------------------")
+    print("Comunicação encerrada")
+    print("-------------------------")
+    com.disable()
+
+if __name__ == "__main__":
+    main()
