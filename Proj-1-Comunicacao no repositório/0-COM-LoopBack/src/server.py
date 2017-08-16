cdf88d136e8fdf54d9b687a7334a9bc32d216a66
eduardotp1 <edu.tirta@gmail.com>
2017-08-16 16:15:02 -0300

    interface

--- Proj-1-Comunicacao no repositório/0-COM-LoopBack/src/server.py
+++ Proj-1-Comunicacao no repositório/0-COM-LoopBack/src/server.py
@@ -16,8 +16,7 @@ import time
 
 #serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
 #serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
-#serialName = "COM3"                  # Windows(variacao de)
-serialName = "COM4"
+serialName = "COM3"                   # Windows(variacao de)
 
 def main():
     # Inicializa enlace
