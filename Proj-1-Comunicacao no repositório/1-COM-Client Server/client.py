16ff086ba530a19c711866c64ee7feba6c0b6c32
IsabellaOInsper <isabellaro@al.insper.edu.br>
2017-09-12 22:29:02 -0300

    Algumas funçoes feitas em testin.py

--- Proj-1-Comunicacao no repositório/1-COM-Client Server/client.py
+++ Proj-1-Comunicacao no repositório/1-COM-Client Server/client.py
@@ -15,10 +15,10 @@ import timeit
 #   para saber a sua porta, execute no terminal :
 #   python -m serial.tools.list_ports
 
-serialName = "/dev/ttyACM2"           # Ubuntu (variacao de)
+#serialName = "/dev/ttyACM2"           # Ubuntu (variacao de)
 #serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
 #serialName = "COM3"                  # Windows(variacao de)
-#serialName = "COM3"
+serialName = "COM4"
 
 def main():
     # Inicializa enlace
